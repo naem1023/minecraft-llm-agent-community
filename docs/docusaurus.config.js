9149b00d9c72185574121251c282399d0f33f899
naem1023 <relilau00@gmail.com>
2024-05-28 01:37:41 +0900

    Update github action manifest

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://naem1023.github.io/minecraft-llm-agent-community',
+  url: 'https://naem1023.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/minecraft-llm-agent-community/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
